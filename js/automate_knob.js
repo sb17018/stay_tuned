ec6c59c82028dcd8bb3dac5b3661c939613aa0d3
Piotr Bar <piotr.a.bar@gmail.com>
2021-05-17 14:41:17 +0100

    Chords knob working. When shifts, the lyrics chords change accordingly.

--- js/automate_knob.js
+++ js/automate_knob.js
@@ -1,10 +1,10 @@
 //Make the DIV element draggable:
-function dragElement(elmnt) {
+function dragElement(elmnt){
   var pos1 = pos2 = 0;
-  var theChordMarks = document.getElementsByClassName('the-chord');
+  var theChordMarks = document.getElementsByClassName('not-empty-chord');
   elmnt.onmousedown = dragMouseDown;
 
-  function dragMouseDown(e) {
+  function dragMouseDown(e){
     e = e || window.event;
     e.preventDefault();
     // get the mouse cursor position at startup:
@@ -13,23 +13,17 @@ function dragElement(elmnt) {
     // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
   }
-
-  function elementDrag(e) {
-    // checkPositionOfNextTone(elmnt);
+    
+  function elementDrag(e){
     e = e || window.event;
     e.preventDefault();
-    // calculate the new cursor position
-    // pos1 = pos2 - e.clientY;
     // set the element's new position:
     pos2 = e.clientY;
-    //console.log("pos2="+pos2);
-    if((pos2 <= 380) && (pos2 >= 160)){
-      //console.log("pos2_3="+pos2);
-      elmnt.style.top = (pos2-70) + "px";
-      //console.log("elmnt.style.top="+(elmnt.style.top));
-      //console.log("elmnt.offsetTop="+(elmnt.offsetTop));
+    if((pos2 <= 505) && (pos2 >= 285)){
+      elmnt.style.top = (pos2 - 200) + "px";
     }
-    colorChosenChord(theChordMarks, elmnt.offsetTop-90);
+    colorChosenChord(theChordMarks, elmnt.offsetTop - 90);
+    adjustChords();
   }
 
   function closeDragElement() {
@@ -44,33 +38,27 @@ function colorChosenChord(theChordMarks, knobPosition){
   for(i = 0; i < theChordMarks.length; i++){
     var chordPositionTop = theChordMarks[i].offsetTop;
     var chordPositionBottom = theChordMarks[i].offsetTop + 54;
-    // console.log("chordPositionTop: " + chordPositionTop);
-    // console.log("chordPositionBottom: " + chordPositionBottom);
-    // console.log("knobPosition+17: " + (knobPosition+17));
-    if(((knobPosition+17)>chordPositionTop)&&((knobPosition+17)<chordPositionBottom)){
-      theChordMarks[i].setAttribute('class', 'the-chord the-chord-current');
-      adjustChords(theChordMarks[i].innerHTML);
+    if(((knobPosition + 17) > chordPositionTop) && ((knobPosition + 17) < chordPositionBottom)){
+      theChordMarks[i].setAttribute('class', 'the-chord not-empty-chord the-chord-current');
     }
     else{
-      theChordMarks[i].setAttribute('class', 'the-chord');
+      theChordMarks[i].setAttribute('class', 'the-chord not-empty-chord');
     }
   }
 }
 
-function adjustChords(chord){
-  var chords = document.getElementsByTagName('span');
-  // //console.log("chord 1: " + chords[0].innerHTML);
-  chords[0].innerHTML = chord;
-  var firstChordPosInTable = chordsTable.indexOf(sessionStorage.getItem("firstChord"));
-  var chosenChordPosInTable = chordsTable.indexOf(chord);
-  var chordsDistance = chosenChordPosInTable - firstChordPosInTable;
-  console.log("firstChordPosInTable: " + firstChordPosInTable);
-  console.log("chosenChordPosInTable: " + chosenChordPosInTable);
-  console.log("chordsDistance: " + chordsDistance);
-  // for(i = 1; i < chords.length; i++){
-  //       var chordPos = chordsTable.indexOf(chords[i].innerHTML);
-  //       chords[i].textContent = 
-  // }
+function adjustChords(){
+  var chordsOverLyrics = document.querySelectorAll('.lyrics-tones span');
+  var lyricsFirstChordPositionInAllChords = chordsTable.indexOf(chordsOverLyrics[0].textContent);
+  var chosenChord = document.querySelector('.the-chord-current');
+  if(chosenChord != null && chosenChord.textContent != chordsOverLyrics[0].textContent){
+    var chosenChordPositionInAllChords = chordsTable.indexOf(chosenChord.textContent);
+    var diffInChords = chosenChordPositionInAllChords - lyricsFirstChordPositionInAllChords;
+    chordsOverLyrics.forEach(e => {
+      var chordPositionInAllChords = chordsTable.indexOf(e.textContent);
+      e.textContent = chordsTable[chordPositionInAllChords + diffInChords];
+    });
+  }
 }
 
 chordsTable = ['A', 'B', 'C', 'D', 'E', 'Em', 'F','G','H'];
